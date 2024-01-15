a09cecf3cb77a43f74402e1c6a5946a416eb116f
Nikita <anosov.nikita@gmail.com>
2024-01-16 01:45:40 +0200

    add design

--- app/src/main/java/com/example/pageflix/registerLibrarian.java
+++ app/src/main/java/com/example/pageflix/registerLibrarian.java
@@ -1,13 +1,13 @@
 package com.example.pageflix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
